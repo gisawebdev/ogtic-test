2fc336ad12762831c508da8cebbb543e9858a37c
Gilbert Acosta <gisawebdev@gmail.com>
2023-05-30 11:25:40 -0400

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,23 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'plugin:react/recommended',
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+	},
+
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		{
+			name: 'eslint-plugin',
+			enforce: 'pre',
+			apply: 'serve',
+			//ts=ignore
+			...require('eslint-webpack-plugin')({
+				context: __dirname,
+				extensions: ['js', 'jsx', 'ts', 'tsx'],
+				files: './src',
+			}),
+		},
+	],
+});
